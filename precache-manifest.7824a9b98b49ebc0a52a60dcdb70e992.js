a97d35913a1db4a52bef57fe5255e98cda6b8c02
huipengpeter <huipengpeter@msn.com>
2020-09-24 15:22:05 -0400

    Updates

---
+++ precache-manifest.7824a9b98b49ebc0a52a60dcdb70e992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27948223ab6a2950badb91979ab0284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e69f2db4361ff50a3b9b",
+    "url": "/static/css/main.ff376301.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e69f2db4361ff50a3b9b",
+    "url": "/static/js/main.1e5c760e.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  }
+]);
